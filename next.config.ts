d728018d5e167470f87d362b94ea3cfbf1b4bcaf
Rachael Arnold <rae.arnold@gmail.com>
2025-10-12 19:54:53 -0400

    fix: throw spaghetti at the wall

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  // assetPrefix: './', // Serve static assets relative to the current path
+  assetPrefix: '/pc-kids-halloween-hunt-2025', // Serve static assets relative to the current path
   basePath: '/pc-kids-halloween-hunt-2025', // Ensure no basePath is set
   distDir: 'docs',
   skipTrailingSlashRedirect: true,
