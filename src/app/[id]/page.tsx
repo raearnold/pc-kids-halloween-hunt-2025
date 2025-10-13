1ccf1d236f33db8d2fde39cfd5b3a5baec297303
Rachael Arnold <rae.arnold@gmail.com>
2025-10-13 08:43:13 -0400

    feat: add rotating icons

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -14,19 +14,22 @@ export async function generateStaticParams() {
   return hintHashMap.map(({ hash }) => ({ id: hash }));
 }
 
-export default function HintPage({ params }: { params: { id: string } }) {
-  const hash = params.id;
-  const hintId = (hashToHintId as Record<string, number>)[hash] ?? null;
-  const hint = hints.find((h) => h.id === hintId);
-  if (!hint) {
+export default async function HintPage({ params }: { params: { id: string } }) {
+  const hash = (await params).id;
+  const hintHashId = (hashToHintId as Record<string, number>)[hash] ?? null;
+  const { id: hintId, text: hintText } = hints.find((h) => h.id === hintHashId) ?? {};
+  if (!hintId) {
     notFound();
   }
+
+  const icons = [ '🦴', '🎃', '🕸️', '🧛‍♂️', '🧟', '🧙', '🦇', '🕷️', '🪦', '🦉', '⚰️', '🧹', '🧑‍🎤',];
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-8 gap-8 text-center">
-      <span className="mb-2 text-[100px]" role="img" aria-label="Jack-o'-lantern">🎃</span>
-      <h1 className="text-3xl font-bold text-orange-700 mb-4">Hint #{hint.id}</h1>
+      <span className="mb-2 text-[100px]" role="presentation">{icons[hintId % icons.length]}</span>
+      <h1 className="text-3xl font-bold text-orange-700 mb-4">Hint #{hintId}</h1>
       <div className="bg-white rounded-xl shadow-lg p-8 border-2 border-orange-200 text-2xl text-center text-gray-800 max-w-xl">
-        {hint.text}
+        {hintText}
       </div>
       <p className="mt-8 text-xs text-gray-400">🎃🎃🎃 Happy Halloween from PC Kids! 🎃🎃🎃 <br/> Scavenger Hunt 2025</p>
     </div>
