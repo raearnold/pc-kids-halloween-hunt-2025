1742109abca2ed5ae6264589584b8a0117458246
Rachael Arnold <rae.arnold@gmail.com>
2025-10-20 18:51:45 -0400

    feat: add answer section

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -17,21 +17,27 @@ export async function generateStaticParams() {
 export default async function HintPage({ params }: { params: { id: string } }) {
   const hash = (await params).id;
   const hintHashId = (hashToHintId as Record<string, number>)[hash] ?? null;
-  const { id: hintId, text: hintText } = hints.find((h) => h.id === hintHashId) ?? {};
+  const { id: hintId, text: hintText, answer: hintAnswer } = hints.find((h) => h.id === hintHashId) ?? {};
   if (!hintId) {
     notFound();
   }
 
-  const icons = [ '🦴', '🎃', '🕸️', '🧛‍♂️', '🧟', '🧙', '🦇', '🕷️', '🪦', '🦉', '⚰️', '🧹', '🧑‍🎤',];
+  const icons = ['🦴', '🎃', '🕸️', '🧛‍♂️', '🧟', '🧙', '🦇', '🕷️', '🪦', '🦉', '⚰️', '🧹', '🧑‍🎤',];
 
-  return (
+  return (<>
     <div className="flex flex-col items-center justify-center min-h-screen p-8 gap-8 text-center">
       <span className="mb-2 text-[100px]" role="presentation">{icons[hintId % icons.length]}</span>
       <h1 className="text-3xl font-bold text-orange-700 mb-4">Hint #{hintId}</h1>
       <div className="bg-white rounded-xl shadow-lg p-8 border-2 border-orange-200 text-2xl text-center text-gray-800 max-w-xl">
         {hintText}
       </div>
-      <p className="mt-8 text-xs text-gray-400">🎃🎃🎃 Happy Halloween from PC Kids! 🎃🎃🎃 <br/> Scavenger Hunt 2025</p>
+      <p className="mt-8 text-xs text-gray-400">🎃🎃🎃  Happy Halloween from PC Kids!  🎃🎃🎃 <br /> Scavenger Hunt 2025</p>
     </div>
+    { hintAnswer &&
+      <div className="min-h-screen p-8 flex justify-center pointer-events-none select-none">
+        <em className="text-xs text-gray-400 rotate-180">{hintAnswer}</em>
+      </div>
+    }
+  </>
   );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Halloween Scavenger Hunt",
@@ -25,7 +14,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-orange-50 text-gray-900 min-h-screen font-sans`}
+        className="antialiased bg-orange-50 text-gray-900 min-h-screen font-sans"
       >
         {children}
       </body>
